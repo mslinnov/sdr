3e9c54a7337b62a5798fbd07a86523ca499658d7
mounir <mounir.lakhfif@gmail.com>
2023-05-14 08:22:56 +0200

    Correction photo joueur test

--- app/Http/Controllers/UserImageController.php
+++ app/Http/Controllers/UserImageController.php
@@ -21,7 +21,7 @@ class UserImageController extends Controller
 
         if ($request->hasFile('images')){
             foreach ($request->file('images') as $file) {
-                $path = $file->store('players', 'public');
+                $path = $file->store('joueur', 'public');
                 // Creation of a new UserImage with the $path of the uploaded image
                 $newImage = new UserImage([
                     'filename' =>$path
